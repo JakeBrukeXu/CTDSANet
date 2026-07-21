f0fe9911fee923ce4cc241dee4ede3321f14e457
Xuzy <66071451+JakeBrukeXu@users.noreply.github.com>
2026-07-21 13:47:55 +0800

    Create layers.py

---
+++ layers.py
@@ -0,0 +1,312 @@
+import math
+import torch
+import torch.nn.functional as F
+from torch.nn.parameter import Parameter
+from torch.nn.modules.module import Module
+import torch.nn as nn
+from einops import rearrange
+
+DEVICE = 'cuda:0' if torch.cuda.is_available() else 'cpu'
+
+
+class GraphConvolution(Module):
+    """
+    simple GCN layer
+    """
+
+    def __init__(self, in_features, out_features, bias=True):
+        super(GraphConvolution, self).__init__()
+        self.in_features = in_features
+        self.out_features = out_features
+        self.weight = Parameter(torch.FloatTensor(in_features, out_features))
+        torch.nn.init.xavier_uniform_(self.weight, gain=1.414)
+        if bias:
+            self.bias = Parameter(torch.zeros((1, 1, out_features), dtype=torch.float32))
+        else:
+            self.register_parameter('bias', None)
+
+    def reset_parameters(self):
+        stdv = 1. / math.sqrt(self.weight.size(1))
+        self.weight.data.uniform_(-stdv, stdv)
+        if self.bias is not None:
+            self.bias.data.uniform_(-stdv, stdv)
+
+    def forward(self, x, adj):
+        output = torch.matmul(x, self.weight) - self.bias
+        output = F.relu(torch.matmul(adj, output))
+        return output
+
+
+# GAM注意力机制
+
+class GAM_Attention(Module):
+    def __init__(self, in_channels, out_channels, rate=4):
+        super(GAM_Attention, self).__init__()
+
+        # 通道注意力
+        self.channel_attention = nn.Sequential(
+            nn.Linear(in_channels, int(in_channels / rate)),
+            nn.ReLU(inplace=True),
+            nn.Linear(int(in_channels / rate), in_channels)
+        )
+
+        # 空间注意力
+        self.spatial_attention = nn.Sequential(
+            nn.Conv2d(in_channels, int(in_channels / rate), kernel_size=7, padding=3),
+            nn.BatchNorm2d(int(in_channels / rate)),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(int(in_channels / rate), out_channels, kernel_size=7, padding=3),
+            nn.BatchNorm2d(out_channels)
+        )
+
+    def forward(self, x):
+        b, c, h, w = x.shape
+        # 输入x为：(64, 32, 1, 512)
+        # x = torch.randn(1, 64, 32, 48)
+        # permutation
+        x_permute = x.permute(0, 2, 3, 1).view(b, -1, c)
+        # x_permute:(64, 512, 32)
+        # print('x_permute的形状：', x_permute.shape)
+        x_att_permute = self.channel_attention(x_permute).view(b, h, w, c)
+        # 输出结果为:(64, 1 512, 32)
+        # print('x_att_permute的形状：', x_att_permute.shape)
+        # reverse permutation
+        x_channel_att = x_att_permute.permute(0, 3, 1, 2)
+        # x_channel_att:(64, 32, 1, 512)
+        # print('x_channel_att的形状：', x_channel_att.shape)
+        x_channel_att = x_channel_att.sigmoid()
+        # print('通道注意力：', x_channel_att)
+        ######################
+        x = x * x_channel_att
+
+        x_spatial_att = self.spatial_attention(x).sigmoid()
+        out = x * x_spatial_att
+        #####################
+
+        return out
+
+
+class GraphConvolution_v1(Module):
+    """
+    simple GCN layer
+    """
+
+    def __init__(self, in_features=64, out_features=64, bias=True):
+        super(GraphConvolution_v1, self).__init__()
+        self.in_features = in_features
+        self.out_features = out_features
+        self.weight = Parameter(torch.FloatTensor(in_features, out_features)).to(DEVICE)
+        torch.nn.init.xavier_uniform_(self.weight, gain=1.414)
+        if bias:
+            self.bias = Parameter(torch.zeros((1, 1, out_features), dtype=torch.float32)).to(DEVICE)
+        else:
+            self.register_parameter('bias', None)
+
+    def reset_parameters(self):
+        stdv = 1. / math.sqrt(self.weight.size(1))
+        self.weight.data.uniform_(-stdv, stdv)
+        if self.bias is not None:
+            self.bias.data.uniform_(-stdv, stdv)
+
+    def forward(self, x, adj):
+        output = torch.matmul(x, self.weight) - self.bias
+        output = F.relu(torch.matmul(adj, output))
+        output = F.relu(output)
+        return output
+
+
+class GraphConvolution_v2(Module):
+    """
+    simple GCN layer
+    """
+
+    def __init__(self, in_features=64, out_features=64, bias=True):
+        super(GraphConvolution_v2, self).__init__()
+        self.in_features = in_features
+        self.out_features = out_features
+        self.weight = Parameter(torch.FloatTensor(in_features, out_features)).to(DEVICE)
+        torch.nn.init.xavier_uniform_(self.weight, gain=1.414)
+        if bias:
+            self.bias = Parameter(torch.zeros((1, 1, out_features), dtype=torch.float32)).to(DEVICE)
+        else:
+            self.register_parameter('bias', None)
+
+    def reset_parameters(self):
+        stdv = 1. / math.sqrt(self.weight.size(1))
+        self.weight.data.uniform_(-stdv, stdv)
+        if self.bias is not None:
+            self.bias.data.uniform_(-stdv, stdv)
+
+    def forward(self, x):
+        output = torch.matmul(x, self.weight) - self.bias
+        output = F.relu(output)
+        return output
+
+
+class GraphConvolution_v3(Module):
+    """
+    simple GCN layer
+    """
+
+    def __init__(self, in_features=64, out_features=64, bias=True):
+        super(GraphConvolution_v3, self).__init__()
+        self.in_features = in_features
+        self.out_features = out_features
+        self.weight = Parameter(torch.FloatTensor(in_features, out_features)).to(DEVICE)
+        torch.nn.init.xavier_uniform_(self.weight, gain=1.414)
+        if bias:
+            self.bias = Parameter(torch.zeros((1, 1, out_features), dtype=torch.float32)).to(DEVICE)
+        else:
+            self.register_parameter('bias', None)
+
+    def reset_parameters(self):
+        stdv = 1. / math.sqrt(self.weight.size(1))
+        self.weight.data.uniform_(-stdv, stdv)
+        if self.bias is not None:
+            self.bias.data.uniform_(-stdv, stdv)
+
+    def forward(self, x):
+        output = torch.matmul(x, self.weight) - self.bias
+        output = F.relu(output)
+        return output
+
+
+class GraphConvolution_v4(Module):
+    """
+    simple GCN layer
+    """
+
+    def __init__(self, in_features=64, out_features=64, bias=True):
+        super(GraphConvolution_v4, self).__init__()
+        self.in_features = in_features
+        self.out_features = out_features
+        self.weight = Parameter(torch.FloatTensor(in_features, out_features)).to(DEVICE)
+        torch.nn.init.xavier_uniform_(self.weight, gain=1.414)
+        if bias:
+            self.bias = Parameter(torch.zeros((1, 1, out_features), dtype=torch.float32)).to(DEVICE)
+        else:
+            self.register_parameter('bias', None)
+
+    def reset_parameters(self):
+        stdv = 1. / math.sqrt(self.weight.size(1))
+        self.weight.data.uniform_(-stdv, stdv)
+        if self.bias is not None:
+            self.bias.data.uniform_(-stdv, stdv)
+
+    def forward(self, x):
+        output = torch.matmul(x, self.weight) - self.bias
+        output = F.relu(output)
+        return output
+
+class PreNorm(nn.Module):
+    def __init__(self, dim, fn):
+        super().__init__()
+        self.norm = nn.LayerNorm(dim)
+        self.fn = fn
+    def forward(self, x, **kwargs):
+        return self.fn(self.norm(x), **kwargs)
+
+class FeedForward(nn.Module):
+    def __init__(self, dim, hidden_dim, dropout = 0.):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(dim, hidden_dim),
+            nn.GELU(),
+            nn.Dropout(dropout),
+            nn.Linear(hidden_dim, dim),
+            nn.Dropout(dropout)
+        )
+    def forward(self, x):
+        return self.net(x)
+class Attention(nn.Module):
+    def __init__(self, dim, heads = 8, dim_head = 64, dropout = 0.):
+        super().__init__()
+        inner_dim = dim_head *  heads
+        project_out = not (heads == 1 and dim_head == dim)
+
+        self.heads = heads
+        self.scale = dim_head ** -0.5
+
+        self.attend = nn.Softmax(dim = -1)
+        self.dropout = nn.Dropout(dropout)
+
+        self.to_qkv = nn.Linear(dim, inner_dim * 3, bias = False)
+
+        self.to_out = nn.Sequential(
+            nn.Linear(inner_dim, dim),
+            nn.Dropout(dropout)
+        ) if project_out else nn.Identity()
+
+    def forward(self, x):
+        qkv = self.to_qkv(x).chunk(3, dim = -1)
+        q, k, v = map(lambda t: rearrange(t, 'b n (h d) -> b h n d', h = self.heads), qkv)
+
+        dots = torch.matmul(q, k.transpose(-1, -2)) * self.scale
+
+        attn = self.attend(dots)
+        attn = self.dropout(attn)
+
+        out = torch.matmul(attn, v)
+        out = rearrange(out, 'b h n d -> b n (h d)')
+        return self.to_out(out)
+
+class Transformer(nn.Module):
+    def __init__(self, dim=1856, depth=6, heads=6, dim_head=64, mlp_dim=64, dropout = 0.5):
+        super().__init__()
+        self.layers = nn.ModuleList([])
+        for _ in range(depth):
+            self.layers.append(nn.ModuleList([
+                PreNorm(dim, Attention(dim, heads = heads, dim_head = dim_head, dropout = dropout)),
+                PreNorm(dim, FeedForward(dim, mlp_dim, dropout = dropout))
+            ]))
+    def forward(self, x):
+        for attn, ff in self.layers:
+            x = attn(x) + x
+            x = ff(x) + x
+        return x
+
+class simam_module(torch.nn.Module):
+    def __init__(self, channels=14, e_lambda=1e-4):
+        super(simam_module, self).__init__()
+
+        self.activaton = nn.Sigmoid()
+        self.e_lambda = e_lambda
+
+    def __repr__(self):
+        s = self.__class__.__name__ + '('
+        s += ('lambda=%f)' % self.e_lambda)
+        return s
+
+    @staticmethod
+    def get_module_name():
+        return "simam"
+
+    def forward(self, x):
+        b, c, h, w = x.size()
+
+        n = w * h - 1
+
+        x_minus_mu_square = (x - x.mean(dim=[2, 3], keepdim=True)).pow(2)
+        y = x_minus_mu_square / (4 * (x_minus_mu_square.sum(dim=[2, 3], keepdim=True) / n + self.e_lambda)) + 0.5
+
+        return x * self.activaton(y)
+
+class Feature_Fusion(Module):
+    def __init__(self, in_channels, out_channels, rate=1):
+        super(Feature_Fusion, self).__init__()
+
+
+        # 空间注意力
+        self.spatial_attention = nn.Sequential(
+            nn.Conv2d(in_channels, int(in_channels / rate), kernel_size=7, padding=3),
+            nn.BatchNorm2d(int(in_channels / rate)),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(int(in_channels / rate), out_channels, kernel_size=7, padding=3),
+            nn.BatchNorm2d(out_channels)
+        )
+
+    def forward(self, x):
+        x_spatial_att = self.spatial_attention(x).sigmoid()
+        out = x * x_spatial_att
+        #####################
+        return out
